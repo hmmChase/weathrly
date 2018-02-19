2e40c06d34d1fbd5be14091fa0249e52c68e3f26
hmmChase <johndoe@example.com>
2018-02-19 11:57:04 -0700

    Add starter files

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  devtool: '#source-map',
+  entry: {
+    main: ['babel-polyfill', './lib/index.js'],
+    test: ['babel-polyfill', 'mocha!./test/index.js'],
+  },
+  output: {
+    path: __dirname,
+    filename: '[name].bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.scss$/, loader: 'style!css!sass' },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'file-loader'
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json', '.scss', '.css'],
+  }
+}
