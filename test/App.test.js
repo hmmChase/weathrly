595cc70b5043fc7794875eaef8d8c9ea2703ba84
hmmChase <johndoe@example.com>
2018-02-24 14:25:48 -0700

    Add test and storageMock for App

---
+++ test/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import App from '../lib/App';
+
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    localStorage.clear();
+    wrapper = shallow(<App />);
+  });
+
+  it('should exist', () => {
+    // console.log(wrapper.debug());
+    expect(wrapper).toBeDefined();
+  });
+});
