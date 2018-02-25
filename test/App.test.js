fd7c6f0f1c077284c734fcf5e6913cb3f0948f22
hmmChase <johndoe@example.com>
2018-02-24 19:26:39 -0700

    Add App render tests

--- test/App.test.js
+++ test/App.test.js
@@ -11,28 +11,51 @@ describe('App', () => {
   });
 
   it('should exist', () => {
-    // console.log(wrapper.debug());
     expect(wrapper).toBeDefined();
-  });
-
-  it('Should render the Search and CurrentWeather components', () => {
-    expect(wrapper.find('Search').length).toEqual(1);
-  });
-
-  it('Should render the Welcome component', () => {
-    expect(wrapper.find('Welcome').length).toEqual(1);
   });
 
   it('Should initially have set states of null for cleanApiData, a set location, and no apiData error', () => {
     expect(wrapper.state()).toEqual(
-      { cleanApiData: null, 
-        location: localStorage.location, 
-        apiDataError: false });
+      {
+        cleanApiData: null,
+        location: localStorage.location,
+        apiDataError: false
+      });
   });
 
-  // it('should set the state of our location', () => {
-  //   wrapper.instance().setLocation('denver')
-  //   expect(wrapper.state('location')).toEqual( 'denver' );
-  // });
-});
+  // IF THERE IS AN ERROR
+  it('Should render the Search components if there is an error', () => {
+    wrapper.setState({ apiDataError: true });
+    expect(wrapper.find('Search').length).toEqual(1);
+  });
 
+  it('Should render the CurrentWeather, SevenHour, and TenDay components if there is an error and there is cleanApiData', () => {
+    wrapper.setState({ apiDataError: true });
+    wrapper.setState({ cleanApiData: true });
+
+    expect(wrapper.find('CurrentWeather').length).toEqual(1);
+    expect(wrapper.find('SevenHour').length).toEqual(1);
+    expect(wrapper.find('TenDay').length).toEqual(1);
+  });
+
+  // IF THERE IS NO LOCATION
+  it('Should render the Search components if there is no location', () => {
+    expect(wrapper.find('Search').length).toEqual(1);
+  });
+
+  it('Should render the Welcome component if there is no location', () => {
+    expect(wrapper.find('Welcome').length).toEqual(1);
+  });
+
+  // IF THERE IS A LOCATION
+  it('Should render the Welcome component if there is no location', () => {
+    wrapper.setState({ location: 'Denver, CO' });
+    wrapper.setState({ cleanApiData: true });
+
+    expect(wrapper.find('CurrentWeather').length).toEqual(1);
+    expect(wrapper.find('SevenHour').length).toEqual(1);
+    expect(wrapper.find('TenDay').length).toEqual(1);
+  });
+
+
+});
